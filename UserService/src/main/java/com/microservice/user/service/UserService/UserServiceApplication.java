ff6c33e70a124f3f9d8468b7cce5a7bf4f4f2a47
anweshaghosh12345 <61232824+anweshaghosh12345@users.noreply.github.com>
2025-04-01 20:24:46 +0530

    all service

--- UserService/src/main/java/com/microservice/user/service/UserService/UserServiceApplication.java
+++ UserService/src/main/java/com/microservice/user/service/UserService/UserServiceApplication.java
@@ -3,14 +3,20 @@ package com.microservice.user.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 
 public class UserServiceApplication {
 
+
+
 	public static void main(String[] args) {
 		System.out.println("App Started");
 		SpringApplication.run(UserServiceApplication.class, args);
+
+
 	}
 
 }
--- UserService/src/main/java/com/microservice/user/service/UserService/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/microservice/user/service/UserService/services/impl/UserServiceImpl.java
@@ -1,25 +1,37 @@
 package com.microservice.user.service.UserService.services.impl;
 
+import com.microservice.user.service.UserService.entities.Rating;
 import com.microservice.user.service.UserService.entities.User;
 import com.microservice.user.service.UserService.exceptions.ResourceNotFoundException;
 import com.microservice.user.service.UserService.repository.UserRepository;
 import com.microservice.user.service.UserService.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
-//    @Autowired
-    private final UserRepository userRepository;
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private  UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+//    private Logger logg= LoggerFactory.getLogger(UserServiceImpl.class);
+
+//    public UserServiceImpl(RestTemplate restTemplate) {
+//        this.restTemplate = restTemplate;
+//    }
+//    public UserServiceImpl(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
 
     @Override
     public User saveUser(User user) {
@@ -30,13 +42,34 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAllUser() {
-        return userRepository.findAll();
+        List<User> alluser=userRepository.findAll();
+        List<User> userSet=new ArrayList<>();
+        for(int i=0;i<alluser.size();i++){
+            User user=alluser.get(i);
+            //        fetch ratings of the above user from rating service
+            ArrayList<Rating> ratingsofUsers=restTemplate.getForObject
+                    ("http://localhost:8083/ratings/users/ratingByUserId/"+user.getUserId(),
+                            ArrayList.class);
+            user.setRating(ratingsofUsers);
+        userSet.add(user);
+        }
+
+        return userSet;
     }
 
     @Override
     public User getUser(Long id) {
-        return userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User with " +
+        User user=userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User with " +
                 "given id is not found on Server !! :"+id));
+//        fetch ratings of the above user from rating service
+//    http://localhost:8083/ratings/users/ratingByUserId/5
+        ArrayList<Rating> ratingsofUsers=restTemplate.getForObject
+                ("http://localhost:8083/ratings/users/ratingByUserId/"+user.getUserId(),
+                ArrayList.class);
+        System.out.println("ratings of users: "+ratingsofUsers);
+
+        user.setRating(ratingsofUsers);
+        return user;
     }
 
     @Override
